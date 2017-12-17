b6b193664ce06f706e865415c5a44b9504dbb008
Maks <maksymles@gmail.com>
2017-12-17 17:07:37 +0100

    Blackjack 2.2 zmiana klasy Cards, połączenie jej z klasą Positionable_Card

--- Blackjack.py
+++ Blackjack.py
@@ -1,14 +1,30 @@
 class Card(object):
+    """Karty do gry"""
     RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     SUITS = ["c", "d", "h", "s"]
 
-    def __init__(self, rank, suit):
+    # def __init__(self, rank, suit):
+    #     self.rank = rank
+    #     self.suit = suit
+    #
+    # def __str__(self):
+    #     rep = self.rank + self.suit
+    #     return rep
+
+    def __init__(self, rank, suit, face_up = True):
         self.rank = rank
         self.suit = suit
+        self.is_face_up = face_up
 
     def __str__(self):
-        rep = self.rank + self.suit
+        if self.is_face_up:
+            rep = self.rank + self.suit
+        else:
+            rep = "XX"
         return rep
+
+    def flip(self):
+        self.is_face_up = not self.is_face_up
 
 class Hand(object):
     """ Ręka gracza  """
@@ -54,39 +70,39 @@ class Deck(Hand):
                 else:
                     print("Nie mogę dalej rozdawać. Zabrakło kart!")
 
-class Positionable_Card(Card):
-    """ Karta, która może być odkryta lub zakryta. """
-    def __init__(self, rank, suit, face_up = True):
-        super(Positionable_Card, self).__init__(rank, suit)
-        self.is_face_up = face_up
+# class Positionable_Card(Card):
+#     """ Karta, która może być odkryta lub zakryta. """
+#     def __init__(self, rank, suit, face_up = True):
+#         super(Positionable_Card, self).__init__(rank, suit)
+#         self.is_face_up = face_up
+#
+#     def __str__(self):
+#         if self.is_face_up:
+#             rep = super(Positionable_Card, self).__str__()
+#         else:
+#             rep = "XX"
+#         return rep
+#
+#     def flip(self):
+#         self.is_face_up = not self.is_face_up
 
-    def __str__(self):
-        if self.is_face_up:
-            rep = super(Positionable_Card, self).__str__()
-        else:
-            rep = "XX"
-        return rep
-
-    def flip(self):
-        self.is_face_up = not self.is_face_up
-
-class Unprintable_Card(Card):
-    """ Karta, której ranga i kolor nie są ujawnione przy jej wyświetleniu. """
-    def __str__(self):
-        return "<utajniona>"
+# class Unprintable_Card(Card):
+#     """ Karta, której ranga i kolor nie są ujawnione przy jej wyświetleniu. """
+#     def __str__(self):
+#         return "<utajniona>"
 
 
 
 
 card1 = Card("A", "c")
-card2 = Unprintable_Card("A", "h")
-card3 = Positionable_Card("A", "s")
+# card2 = Unprintable_Card("A", "h")
+card3 = Card("A", "s")
 
 print("Wyświetlenie obiektu klasy Card: ")
 print(card1)
 
-print("\nWyświetlanie obiektu klasy Unprintable_Card: ")
-print(card2)
+# print("\nWyświetlanie obiektu klasy Unprintable_Card: ")
+# print(card2)
 
 print("\nWyświetlanie obiektu klasy positionable_Card: ")
 print(card3)
